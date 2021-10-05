faa490c4de15c23401525ebf6ad532e2bd724e5e
Jonas Carson <jonascarson@gmail.com>
2021-10-04 18:51:54 -0700

    -- first commit, basic cookiecutter

---
+++ gac_django/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from gac_django.users.models import User
+from gac_django.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
