93a84279eed06d2f04eb9afd2f9bf14314dd75f0
Jonas Carson <jcarson@uw.edu>
2022-06-13 11:26:49 -0700

    Update logging settings for apps-dev site

--- config/settings/apps_dev.py
+++ config/settings/apps_dev.py
@@ -4,10 +4,13 @@ from .production import LOGGING, env  # noqa
 # Log to file if we are in mod_wsgi. How to determine if in mod_wsgi
 # https://modwsgi.readthedocs.io/en/develop/user-guides/assorted-tips-and-tricks.html#determining-if-running-under-mod-wsgi
 
+LOGGING["root"]["level"] = "DEBUG"
+
 try:
     from mod_wsgi import version  # noqa
 
     LOGGING["handlers"]["console"]["class"] = "logging.FileHandler"
     LOGGING["handlers"]["console"]["filename"] = "/var/log/django/primed-app-dev.log"
+
 except ImportError:
     LOGGING["handlers"]["console"]["class"] = "logging.StreamHandler"
