92558c421e4710ec5c0a879aeeea4bab58ff016b
Adrienne Stilp <amstilp@uw.edu>
2022-08-26 15:03:30 -0700

    Fix model and factory type for dbGaPWorkspaceTableTest in tests
    Had accidentally specified the dbGaPWorkspace as the model instead
    of the Workspace.

--- primed/primed_anvil/tests/test_tables.py
+++ primed/primed_anvil/tests/test_tables.py
@@ -1,3 +1,5 @@
+from anvil_consortium_manager import models as acm_models
+from anvil_consortium_manager.tests import factories as acm_factories
 from django.test import TestCase
 
 from .. import models, tables
@@ -25,8 +27,8 @@ class StudyTableTest(TestCase):
 
 
 class dbGaPWorkspaceTableTest(TestCase):
-    model = models.dbGaPWorkspace
-    model_factory = factories.dbGaPWorkspaceFactory
+    model = acm_models.Workspace
+    model_factory = acm_factories.WorkspaceFactory
     table_class = tables.dbGaPWorkspaceTable
 
     def test_row_count_with_no_objects(self):
@@ -34,11 +36,11 @@ class dbGaPWorkspaceTableTest(TestCase):
         self.assertEqual(len(table.rows), 0)
 
     def test_row_count_with_one_object(self):
-        self.model_factory.create()
+        self.model_factory.create(workspace_type="dbgap")
         table = self.table_class(self.model.objects.all())
         self.assertEqual(len(table.rows), 1)
 
     def test_row_count_with_two_objects(self):
-        self.model_factory.create_batch(2)
+        self.model_factory.create_batch(2, workspace_type="dbgap")
         table = self.table_class(self.model.objects.all())
         self.assertEqual(len(table.rows), 2)
