482faf5aa1d1a0c6a9f237eed13e0f1e126d1924
Adrienne Stilp <amstilp@uw.edu>
2022-09-28 16:28:51 -0700

    Add a table for dbGaPWorkspaces workspace data

---
+++ primed/dbgap/tables.py
@@ -0,0 +1,27 @@
+"""Tables for the `dbgap` app."""
+
+import django_tables2 as tables
+from anvil_consortium_manager.models import Workspace
+
+
+class dbGaPWorkspaceTable(tables.Table):
+    """A table for Workspaces that includes fields from dbGaPWorkspace."""
+
+    name = tables.columns.Column(linkify=True)
+    dbgapworkspace__study = tables.columns.Column(linkify=True)
+
+    class Meta:
+        model = Workspace
+        fields = (
+            "name",
+            "dbgapworkspace__study",
+            "dbgapworkspace__phs",
+            "dbgapworkspace__version",
+            "dbgapworkspace__full_consent_code",
+        )
+
+    def render_dbgapworkspace__phs(self, value):
+        return "phs{0:06d}".format(value)
+
+    def render_dbgapworkspace__version(self, value):
+        return "v{}".format(value)
