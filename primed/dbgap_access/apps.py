7296380d2bb9eef906aa90bda66fe288e7e7e41f
Adrienne Stilp <amstilp@uw.edu>
2022-09-28 15:24:11 -0700

    Remove accidental app added in previous branch

--- primed/dbgap_access/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class DbgapAccessConfig(AppConfig):
-    """Configuration for the dbgap_access app."""
-
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "primed_anvil.dbgap_access"
-    verbose_name = " dbGaP Access"
