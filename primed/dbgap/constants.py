16477b0798ee7570c1b73a302f16b3ff93ea3f12
Adrienne Stilp <amstilp@uw.edu>
2022-10-18 17:06:49 -0700

    Split dbGaP DAR JSON validation into multiple vs single projects
    The single project JSON validates the JSON entry for a single
    project (not as an array).

--- primed/dbgap/constants.py
+++ primed/dbgap/constants.py
@@ -1,3 +1,46 @@
+json_dar_defs = {
+    "project": {
+        "required": ["Project_id", "studies"],
+        "type": "object",
+        "properties": {
+            "Project_id": {
+                "description": "dbGaP-assigned unique identifier for this application.",
+                "type": "number",
+            },
+            "PI_name": {
+                "description": "Name of the principal investigator.",
+                "type": "string",
+            },
+            "Project_closed": {
+                "description": "Is the project closed?",
+                "type": "string",
+            },
+            "studies": {"type": "array", "items": {"$ref": "#/$defs/study"}},
+        },
+    },
+    "study": {
+        "required": ["study_accession", "requests"],
+        "type": "object",
+        "properties": {
+            "study_name": {"type": "string"},
+            "study_accession": {"type": "string"},
+            "requests": {"type": "array", "items": {"$ref": "#/$defs/request"}},
+        },
+    },
+    "request": {
+        "type": "object",
+        "required": ["consent_abbrev", "consent_code", "DAR", "current_DAR_status"],
+        "properties": {
+            "consent_abbrev": {"type": "string"},
+            "consent_code": {"type": "number"},
+            "DAR": {"type": "number"},
+            "current_DAR_status": {"type": "string"},
+        },
+    },
+}
+
+
+# Validation of dbGaP DAR JSON from their web service (a array of multiple project DARs).
 json_dar_schema = {
     "title": "dbGaP DAR schema",
     "description": "json schema for the dbGaP DAR data",
@@ -5,44 +48,13 @@ json_dar_schema = {
     "items": {"$ref": "#/$defs/project"},
     "minItems": 1,
     "maxItems": 1,
-    "$defs": {
-        "project": {
-            "required": ["Project_id", "studies"],
-            "type": "object",
-            "properties": {
-                "Project_id": {
-                    "description": "dbGaP-assigned unique identifier for this application.",
-                    "type": "number",
-                },
-                "PI_name": {
-                    "description": "Name of the principal investigator.",
-                    "type": "string",
-                },
-                "Project_closed": {
-                    "description": "Is the project closed?",
-                    "type": "string",
-                },
-                "studies": {"type": "array", "items": {"$ref": "#/$defs/study"}},
-            },
-        },
-        "study": {
-            "required": ["study_accession", "requests"],
-            "type": "object",
-            "properties": {
-                "study_name": {"type": "string"},
-                "study_accession": {"type": "string"},
-                "requests": {"type": "array", "items": {"$ref": "#/$defs/request"}},
-            },
-        },
-        "request": {
-            "type": "object",
-            "required": ["consent_abbrev", "consent_code", "DAR", "current_DAR_status"],
-            "properties": {
-                "consent_abbrev": {"type": "string"},
-                "consent_code": {"type": "number"},
-                "DAR": {"type": "number"},
-                "current_DAR_status": {"type": "string"},
-            },
-        },
-    },
+    "$defs": json_dar_defs,
+}
+
+# Validation fo dbGaP DAR JSON for a single project (one entry in what's returned by their web servce).
+json_dar_schema_one_project = {
+    "title": "dbGaP Project DAR schema",
+    "description": "json schema for DARs for a single dbGaP project",
+    "$ref": "#/$defs/project",
+    "$defs": json_dar_defs,
 }
