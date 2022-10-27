64c5eefc2ced77e7cef7baf2339be221eb5df05a
Jonas Carson <jcarson@uw.edu>
2022-10-27 09:27:05 -0700

    Update study factory to create unique sequenced short_names so create_batch will create requested number of objects consistently. Prev use of get_or_create with limited FAker word led to name clashes so in some cases fewere objects were created than requested.

--- primed/primed_anvil/tests/factories.py
+++ primed/primed_anvil/tests/factories.py
@@ -7,12 +7,11 @@ from .. import models
 class StudyFactory(DjangoModelFactory):
     """A factory for the Study model."""
 
-    short_name = Faker("word")
+    short_name = Sequence(lambda n: "Study:{0:07d}".format(n))
     full_name = Faker("company")
 
     class Meta:
         model = models.Study
-        django_get_or_create = ["short_name"]
 
 
 class DataUsePermissionFactory(DjangoModelFactory):
