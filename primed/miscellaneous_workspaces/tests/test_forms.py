be8124f9f0798d934a49c5dbc3eb3fa21e68c88d
Adrienne Stilp <amstilp@uw.edu>
2022-11-29 16:53:52 -0800

    Add tests for ConsortiumDevelWorkspaceForm

--- primed/miscellaneous_workspaces/tests/test_forms.py
+++ primed/miscellaneous_workspaces/tests/test_forms.py
@@ -31,3 +31,30 @@ class SimulatedDataWorkspaceFormTest(TestCase):
         self.assertIn("workspace", form.errors)
         self.assertEqual(len(form.errors["workspace"]), 1)
         self.assertIn("required", form.errors["workspace"][0])
+
+
+class ConsortiumDevelWorkspaceFormTest(TestCase):
+
+    form_class = forms.ConsortiumDevelWorkspaceForm
+
+    def setUp(self):
+        """Create a workspace for use in the form."""
+        self.workspace = WorkspaceFactory.create()
+
+    def test_valid(self):
+        """Form is valid with necessary input."""
+        form_data = {
+            "workspace": self.workspace,
+        }
+        form = self.form_class(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_missing_workspace(self):
+        """Form is invalid when missing workspace."""
+        form_data = {}
+        form = self.form_class(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+        self.assertIn("workspace", form.errors)
+        self.assertEqual(len(form.errors["workspace"]), 1)
+        self.assertIn("required", form.errors["workspace"][0])
