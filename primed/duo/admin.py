52b610acee916d715ca2ceb037f2acb572bece4e
Adrienne Stilp <amstilp@uw.edu>
2022-12-16 09:39:41 -0800

    Add a new "duo" app with commented out code with DUO models

---
+++ primed/duo/admin.py
@@ -0,0 +1,43 @@
+# from django.contrib import admin
+
+# Register your models here.
+# @admin.register(models.DataUsePermission)
+# class DataUsePermissionAdmin(SimpleHistoryAdmin):
+#     """Admin class for the `DataUsePermission` model."""
+#
+#     list_display = (
+#         "code",
+#         "identifier",
+#         "description",
+#     )
+#     search_fields = (
+#         "code",
+#         "identifier",
+#         "description",
+#     )
+#     sortable_by = (
+#         "code",
+#         "identifier",
+#         "description",
+#     )
+#
+#
+# @admin.register(models.DataUseModifier)
+# class DataUseModifierAdmin(SimpleHistoryAdmin):
+#     """Admin class for the `DataUseModifier` model."""
+#
+#     list_display = (
+#         "code",
+#         "identifier",
+#         "description",
+#     )
+#     search_fields = (
+#         "code",
+#         "identifier",
+#         "description",
+#     )
+#     sortable_by = (
+#         "code",
+#         "identifier",
+#         "description",
+#     )
