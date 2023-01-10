93f2d7418bd56b6e509d1a74e0c8cdf6ddfe977f
Adrienne Stilp <amstilp@uw.edu>
2023-01-10 10:59:40 -0800

    Add a table for OpenAccessWorkspaces

---
+++ primed/miscellaneous_workspaces/tables.py
@@ -0,0 +1,17 @@
+"""Tables for the `miscellaneous_workspace` app."""
+
+import django_tables2 as tables
+from anvil_consortium_manager.models import Workspace
+
+
+class OpenAccessWorkspaceTable(tables.Table):
+    """Class to render a table of Workspace objects with OpenAccessWorkspace workspace data."""
+
+    name = tables.columns.Column(linkify=True)
+
+    class Meta:
+        model = Workspace
+        fields = (
+            "name",
+            "openaccessworkspace__studies",
+        )
