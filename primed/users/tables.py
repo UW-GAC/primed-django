1724ee3c248db8194a01276a868490fbb2bd61b5
Jonas Carson <jcarson@uw.edu>
2023-01-13 12:13:01 -0800

    Additional changes to show site users on studysite detail page. Add corresponding tests

--- primed/users/tables.py
+++ primed/users/tables.py
@@ -1,8 +1,8 @@
 import django_tables2 as tables
-
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
 
 class UserTable(tables.Table):
     """A table for `User`s."""
@@ -11,4 +11,4 @@ class UserTable(tables.Table):
 
     class Meta:
         model = User
-        fields = ("username", "name", "email")
+        fields = ("username", "name", "email", "study_sites")
