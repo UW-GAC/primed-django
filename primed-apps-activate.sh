a8153687854c7a19441796d91e090dbbb26af926
Jonas Carson <jcarson@uw.edu>
2023-03-24 10:07:04 -0700

    Add app activation scripts and cron entries for anvil audit for primed

---
+++ primed-apps-activate.sh
@@ -0,0 +1,7 @@
+export DJANGO_SITE_DIR=/var/www/django/primed_apps/
+export DJANGO_SITE_USER=primedweb
+export DJANGO_SETTINGS_MODULE=config.settings.apps
+export DJANGO_WSGI_FILE=config/primed_apps_wsgi.py
+export GAC_ENV=primed_apps
+cd $DJANGO_SITE_DIR
+. venv/bin/activate
---
+++ primed-apps-dev-activate.sh
@@ -0,0 +1,11 @@
+# Sets environment variables, changes directory and activates venv
+# for gregor apps dev env
+# Used by crontab, validate any changes
+
+export DJANGO_SITE_DIR=/var/www/django/primed_apps_dev/
+export DJANGO_SITE_USER=primedweb
+export DJANGO_SETTINGS_MODULE=config.settings.apps_dev
+export DJANGO_WSGI_FILE=config/primed_apps_dev_wsgi.py
+export GAC_ENV=primed_apps_dev
+cd $DJANGO_SITE_DIR
+. venv/bin/activate
