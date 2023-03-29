3e81cb4c7f03582e44fe02957dce8203d5915e4a
Adrienne Stilp <amstilp@uw.edu>
2023-03-29 10:46:35 -0700

    Add a first pass CDSA model for signed agreements
    No migration yet because I'm not sure if this is the way we'll
    represent CDSAs yet.

--- primed/cdsa/models.py
+++ primed/cdsa/models.py
@@ -1,3 +1,51 @@
-# from django.db import models
+from anvil_consortium_manager import ManagedGroup
+from django.conf import settings
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+
+# Consider splitting this into separate models for different CDSA types.
+class SignedAgreement(TimeStampedModel, models.Model):
+    """A model to track verified CDSAs."""
+
+    MEMBER = "member"
+    DATA_AFFILIATE = "data_affiliate"
+    NON_DATA_AFFILIATE = "non_data_affiliate"
+    CATEGORY_CHOICES = (
+        (MEMBER, "Member"),
+        (DATA_AFFILIATE, "Data affiliate"),
+        (NON_DATA_AFFILIATE, "Non-data affiliate"),
+    )
+
+    cc_id = models.IntegerField(
+        help_text="Identifier assigned by the CC.",
+        unique=True,
+    )
+    representative = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        help_text="The investigator who signed this CDSA.",
+    )
+    # We don't have an institution model so this has to be free text.
+    instutition = models.CharField(max_length=255)
+    category = models.CharField(
+        verbose_name="CDSA category",
+        max_length=31,
+        choices=CATEGORY_CHOICES,
+    )
+    is_component = models.BooleanField()
+    # TODO: This is ambiguously named.
+    group = models.CharField(
+        help_text="Study site, study, or center that the CDSA is associated with."
+    )
+    representatitive_role = models.CharField(
+        help_text="Representative's role in the group."
+    )
+    anvil_access_group = models.OneToOneField(
+        ManagedGroup, verbose_name=" AnVIL access group"
+    )
+
+    def clean(self):
+        """Custom validation checks."""
+        # To add?
+        # - Dealing with component? Is this worth it? Or have a foreign key to self to the non-component?
