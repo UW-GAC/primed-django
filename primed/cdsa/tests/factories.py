9da45da8fdcbca6a35a87bed5dcb087c1774b15c
Adrienne Stilp <amstilp@uw.edu>
2023-03-30 10:41:36 -0700

    Updates to factories and CDSA test data script

--- primed/cdsa/tests/factories.py
+++ primed/cdsa/tests/factories.py
@@ -4,11 +4,13 @@ from anvil_consortium_manager.tests.factories import (
 )
 from factory import (  # Dict,; DictFactory,; LazyAttribute,; List,; SelfAttribute,; Trait,; post_generation,
     Faker,
+    LazyAttribute,
     Sequence,
     SubFactory,
 )
 from factory.django import DjangoModelFactory
 
+from primed.duo.tests.factories import DataUsePermissionFactory
 from primed.primed_anvil.tests.factories import StudyFactory
 from primed.users.tests.factories import UserFactory
 
@@ -25,7 +27,12 @@ class CDSAFactory(DjangoModelFactory):
     type = models.CDSA.MEMBER
     is_component = False
     representative_role = Faker("job")
-    anvil_access_group = SubFactory(ManagedGroupFactory)
+    anvil_access_group = SubFactory(
+        ManagedGroupFactory,
+        name=LazyAttribute(
+            lambda o: "PRIMED_CDSA_ACCESS_{}".format(o.factory_parent.cc_id)
+        ),
+    )
 
     class Meta:
         model = models.CDSA
@@ -38,8 +45,8 @@ class CDSAWorkspaceFactory(DjangoModelFactory):
     requested_by = SubFactory(UserFactory)
     cdsa = SubFactory(CDSAFactory)
     study = SubFactory(StudyFactory)
-
-    data_use_limitations = Faker("paragraph")
+    data_use_permission = SubFactory(DataUsePermissionFactory)
+    data_use_limitations = Faker("paragraph", nb_sentences=5)
     acknowledgments = Faker("paragraph")
 
     class Meta:
