0a3371305e3b79fface04513f8ff294d6d8f8b93
Adrienne Stilp <amstilp@uw.edu>
2023-04-20 15:25:26 -0700

    Add AgreementVersion to admin interface

--- primed/cdsa/admin.py
+++ primed/cdsa/admin.py
@@ -4,6 +4,25 @@ from simple_history.admin import SimpleHistoryAdmin
 from . import models
 
 
+@admin.register(models.AgreementVersion)
+class AgreementVersion(SimpleHistoryAdmin):
+    """Admin class for the `AgreementVersion` model."""
+
+    list_display = (
+        "full_version",
+        "major_version",
+        "minor_version",
+        "date_approved",
+    )
+    list_filter = (
+        "major_version",
+    )
+    sortable_by = (
+        "major_version",
+        "minor_version",
+        "date_approved",
+    )
+
 # @admin.register(models.SignedAgreement)
 # class SignedAgreement(SimpleHistoryAdmin):
 #     """Admin class for the `SignedAgreement` model."""
