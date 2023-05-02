5b014fb04f57afaf13a0315299de7228da868c44
Adrienne Stilp <amstilp@uw.edu>
2023-05-02 09:49:56 -0700

    Add a placeholder for CDSA workspace reports url

--- primed/cdsa/urls.py
+++ primed/cdsa/urls.py
@@ -64,6 +64,12 @@ records_patterns = (
             views.UserAccessRecords.as_view(),
             name="user_access",
         ),
+        # To add once we add CDSA workspaces.
+        # path(
+        #     "workspaces/",
+        #     views.WorkspaceRecords.as_view(),
+        #     name="workspaces",
+        # ),
     ],
     "records",
 )
