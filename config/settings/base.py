16d82c540f341e9ef2746086b886ca84543e9bb8
Jonas Carson <jcarson@uw.edu>
2023-06-08 11:41:38 -0700

    Change admin email to primedweb

--- config/settings/base.py
+++ config/settings/base.py
@@ -246,7 +246,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Jonas Carson""", "jcarson@uw.edu")]
+ADMINS = [("""Primed Admins""", "primedweb@uw.edu")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
