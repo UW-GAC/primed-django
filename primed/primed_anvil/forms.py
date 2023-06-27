4cf0d182af50de4637445f027979391c2b44e71e
Wienwipa Kirdpoo <wkirdpoo@uw.edu>
2023-06-26 21:51:18 -0700

    Add UserSearchForm using UserSearchAutocomplete view Add tests for the form.

--- primed/primed_anvil/forms.py
+++ primed/primed_anvil/forms.py
@@ -1,7 +1,32 @@
+from dal import autocomplete
 from django import forms
+from django.contrib.auth import get_user_model
 
 
 class CustomDateInput(forms.widgets.DateInput):
     """Form widget to select a date with a calendar picker."""
 
     input_type = "date"
+
+
+class UserSearchForm(forms.ModelForm):
+    """Form for the user search"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["name"].required = True
+
+    class Meta:
+        model = get_user_model()
+        fields = ("name",)
+
+        widgets = {
+            "name": autocomplete.ListSelect2(
+                url="gregor_anvil:user:autocomplete",
+                attrs={"data-theme": "bootstrap-5"},
+            ),
+        }
+
+        help_texts = {
+            "name": "Enter either the name or username to search",
+        }
