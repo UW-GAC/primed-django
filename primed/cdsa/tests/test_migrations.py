8c5b4ce9eea4d724c619061c5a79de7c7adf9453
Adrienne Stilp <amstilp@uw.edu>
2023-08-31 16:20:25 -0700

    Modify the migration test to cover the set of related migrations
    Instead of only migrating from before the data migration to immediately
    after, migrate from the first migration before any of the changes
    (initial) to the last migration in the set (0007) in tests. This is
    a better test of what is actually happening (and what we actually
    want to happen).

--- primed/cdsa/tests/test_migrations.py
+++ primed/cdsa/tests/test_migrations.py
@@ -7,11 +7,11 @@ import factory
 
 from . import factories
 
-class PopulateAgreementMajorVersionTest(MigratorTestCase):
-    """Tests for the populate_agreement_major_version migration."""
+class AgreementMajorVersionMigrationsTest(MigratorTestCase):
+    """Tests for the migrations associated with creating the new AgreementMajorVersion model."""
 
-    migrate_from = ("cdsa", "0003_agreementversion_add_major_version_fk")
-    migrate_to = ("cdsa", "0004_populate_agreementmajorversion")
+    migrate_from = ("cdsa", "0001_initial")
+    migrate_to = ("cdsa", "0007_alter_agreementversion_major_version")
 
     def prepare(self):
         """Prepare some data before the migration."""
@@ -60,36 +60,43 @@ class PopulateAgreementMajorVersionTest(MigratorTestCase):
         major_version = AgreementMajorVersion.objects.get(version=5)
         major_version.full_clean()
 
-    def test_agreement_version_major_version_fk_correctly_populated(self):
-        """AgreementVersion.major_version_fk is correctly populated."""
+    def test_agreement_version_major_version_correctly_populated(self):
+        """AgreementVersion.major_version is correctly populated."""
         AgreementMajorVersion = self.new_state.apps.get_model("cdsa", "AgreementMajorVersion")
         AgreementVersion = self.new_state.apps.get_model("cdsa", "AgreementVersion")
         # Major version 1.
         major_version = AgreementMajorVersion.objects.get(version=1)
         agreement_version = AgreementVersion.objects.get(pk=self.agreement_version_1_0.pk)
-        self.assertEqual(agreement_version.major_version_fk, major_version)
+        agreement_version.full_clean()
+        self.assertEqual(agreement_version.major_version, major_version)
         self.assertEqual(agreement_version.minor_version, 0)
         agreement_version = AgreementVersion.objects.get(pk=self.agreement_version_1_1.pk)
-        self.assertEqual(agreement_version.major_version_fk, major_version)
+        agreement_version.full_clean()
+        self.assertEqual(agreement_version.major_version, major_version)
         self.assertEqual(agreement_version.minor_version, 1)
         agreement_version = AgreementVersion.objects.get(pk=self.agreement_version_1_2.pk)
-        self.assertEqual(agreement_version.major_version_fk, major_version)
+        agreement_version.full_clean()
+        self.assertEqual(agreement_version.major_version, major_version)
         self.assertEqual(agreement_version.minor_version, 2)
         # Major version 2.
         major_version = AgreementMajorVersion.objects.get(version=2)
         agreement_version = AgreementVersion.objects.get(pk=self.agreement_version_2_1.pk)
-        self.assertEqual(agreement_version.major_version_fk, major_version)
+        agreement_version.full_clean()
+        self.assertEqual(agreement_version.major_version, major_version)
         self.assertEqual(agreement_version.minor_version, 1)
         agreement_version = AgreementVersion.objects.get(pk=self.agreement_version_2_3.pk)
-        self.assertEqual(agreement_version.major_version_fk, major_version)
+        agreement_version.full_clean()
+        self.assertEqual(agreement_version.major_version, major_version)
         self.assertEqual(agreement_version.minor_version, 3)
         # Major version 3.
         major_version = AgreementMajorVersion.objects.get(version=3)
         agreement_version = AgreementVersion.objects.get(pk=self.agreement_version_3_0.pk)
-        self.assertEqual(agreement_version.major_version_fk, major_version)
+        agreement_version.full_clean()
+        self.assertEqual(agreement_version.major_version, major_version)
         self.assertEqual(agreement_version.minor_version, 0)
         # Major version 5.
         major_version = AgreementMajorVersion.objects.get(version=5)
         agreement_version = AgreementVersion.objects.get(pk=self.agreement_version_5_6.pk)
-        self.assertEqual(agreement_version.major_version_fk, major_version)
+        agreement_version.full_clean()
+        self.assertEqual(agreement_version.major_version, major_version)
         self.assertEqual(agreement_version.minor_version, 6)
