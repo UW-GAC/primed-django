af0916cea3dae517bea35d64e868649127aa8b89
Adrienne Stilp <amstilp@uw.edu>
2023-10-03 15:44:04 -0700

    Add tests for the StudyList view
    For some reason there were no tests for this view, but coverage
    was still 100%. Huh. Maybe because it is a class-based view without
    much customization?

--- primed/primed_anvil/tests/test_views.py
+++ primed/primed_anvil/tests/test_views.py
@@ -567,6 +567,100 @@ class StudyCreateTest(TestCase):
         self.assertEqual(models.Study.objects.count(), 0)
 
 
+class StudyListTest(TestCase):
+    """Tests for the StudyList view."""
+
+    def setUp(self):
+        """Set up test class."""
+        self.factory = RequestFactory()
+        self.model_factory = factories.StudyFactory
+        # Create a user with both view and edit permission.
+        self.user = User.objects.create_user(username="test", password="test")
+        self.user.user_permissions.add(
+            Permission.objects.get(
+                codename=acm_models.AnVILProjectManagerAccess.VIEW_PERMISSION_CODENAME
+            )
+        )
+
+    def get_url(self):
+        """Get the url for the view being tested."""
+        return reverse("primed_anvil:studies:list")
+
+    def get_view(self):
+        """Return the view being tested."""
+        return views.StudyList.as_view()
+
+    def test_view_redirect_not_logged_in(self):
+        "View redirects to login view when user is not logged in."
+        # Need a client for redirects.
+        response = self.client.get(self.get_url())
+        self.assertRedirects(
+            response, resolve_url(settings.LOGIN_URL) + "?next=" + self.get_url()
+        )
+
+    def test_view_render(self):
+        obj = self.model_factory.create()
+        self.client.force_login(self.user)
+        response = self.client.get(self.get_url())
+
+        self.assertContains(response, obj.short_name)
+        self.assertTemplateUsed(response, "primed_anvil/study_list.html")
+
+    def test_status_code_with_user_permission(self):
+        """Returns successful response code."""
+        request = self.factory.get(self.get_url())
+        request.user = self.user
+        response = self.get_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_access_without_user_permission(self):
+        """Raises permission denied if user has no permissions."""
+        user_no_perms = User.objects.create_user(
+            username="test-none", password="test-none"
+        )
+        request = self.factory.get(self.get_url())
+        request.user = user_no_perms
+        with self.assertRaises(PermissionDenied):
+            self.get_view()(request)
+
+    def test_view_has_correct_table_class(self):
+        """View has the correct table class in the context."""
+        request = self.factory.get(self.get_url())
+        request.user = self.user
+        response = self.get_view()(request)
+        self.assertIn("table", response.context_data)
+        self.assertIsInstance(response.context_data["table"], tables.StudyTable)
+
+    def test_view_with_no_objects(self):
+        """The table has no rows when there are no Study objects."""
+        request = self.factory.get(self.get_url())
+        request.user = self.user
+        response = self.get_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("table", response.context_data)
+        self.assertEqual(len(response.context_data["table"].rows), 0)
+
+    def test_view_with_one_object(self):
+        """The table has one row when there is one Study object."""
+        self.model_factory.create()
+        request = self.factory.get(self.get_url())
+        request.user = self.user
+        response = self.get_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("table", response.context_data)
+        self.assertEqual(len(response.context_data["table"].rows), 1)
+
+    def test_view_with_two_objects(self):
+        """The table has two rows when there are two Study objects."""
+        self.model_factory.create_batch(2)
+        request = self.factory.get(self.get_url())
+        request.user = self.user
+        response = self.get_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("table", response.context_data)
+        self.assertEqual(len(response.context_data["table"].rows), 2)
+
+
 class StudySiteDetailTest(TestCase):
     """Tests for the StudySiteDetail view."""
 
