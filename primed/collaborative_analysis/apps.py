a563f29c6b844cbb9e6a470cd16b563583f35e67
Adrienne Stilp <amstilp@uw.edu>
2023-12-04 16:02:32 -0800

    Rename app to collaborative_analysis

--- primed/collaborative_analysis/apps.py
+++ primed/collaborative_analysis/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CollabAnalysisConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "primed.collab_analysis"
+    name = "primed.collaborative_analysis"
