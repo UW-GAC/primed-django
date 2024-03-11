40f1270453e8f2a745fdc43845d0f654cac0b1fe
Adrienne Stilp <amstilp@uw.edu>
2024-03-11 15:57:46 -0700

    Add javascript to check length of pasted text
    In form input fields with the maxlength attribute, add custom
    javascript that prevents pasting text with more than maxlength
    characters. This can help prevent data truncation when pasting more
    characters than maxlength allows, since by default they would be
    silently truncated.

--- primed/static/js/project.js
+++ primed/static/js/project.js
@@ -1,1 +1,15 @@
 /* Project specific Javascript goes here. */
+
+// Handle paste event for text inputs with maxlength.
+const checkPasteLength = (e) => {
+	var paste = (event.clipboardData || window.clipboardData).getData("text");
+	maxlength = e.target.getAttribute("maxlength");
+  if (maxlength <= paste.length) {
+    alert("String longer than allowed maximum length of " + maxlength + " characters:\n" + paste)
+    e.preventDefault()
+    e.stopPropagation()
+  }
+}
+
+var textInputs = $('form').find("input[maxlength]")
+textInputs.on("paste", checkPasteLength);
