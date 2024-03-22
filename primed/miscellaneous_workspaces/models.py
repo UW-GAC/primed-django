514425cb6af1683ba08fff5e93095a0162bc74bf
Adrienne Stilp <amstilp@uw.edu>
2024-03-22 16:14:41 -0700

    Change intended_workspace_type to intended_usage
    For TemplateWorkspaces, remove the intended_workspace_type field and
    add an intended_usage field instead. This is helpful because a
    template workspace can be cloned to create one of a couple workspace
    types (eg, the genotype template is used for both CDSA and dbGaP).

--- primed/miscellaneous_workspaces/models.py
+++ primed/miscellaneous_workspaces/models.py
@@ -1,6 +1,5 @@
 """Model definitions for the `miscellaneous_workspaces` app."""
 
-from anvil_consortium_manager.adapters.workspace import workspace_adapter_registry
 from anvil_consortium_manager.models import BaseWorkspaceData, Workspace
 from django.core.exceptions import ValidationError
 from django.db import models
@@ -24,28 +23,7 @@ class ResourceWorkspace(RequesterModel, TimeStampedModel, BaseWorkspaceData):
 class TemplateWorkspace(TimeStampedModel, BaseWorkspaceData):
     """A model to track template workspaces."""
 
-    intended_workspace_type = models.CharField(max_length=63)
-
-    def clean(self):
-        """Custom cleaning checks.
-
-        - Verify that intended_workspace_type is one of the registered types, excluding this type."""
-        registered_workspace_types = workspace_adapter_registry.get_registered_names()
-        if self.intended_workspace_type:
-            if self.intended_workspace_type not in registered_workspace_types:
-                raise ValidationError(
-                    {
-                        "intended_workspace_type": "intended_workspace_type must be one of the registered types."
-                    }
-                )
-            # We cannot import the adapter here because it would lead to a circular import, but we don't want
-            # to create a template workspace for TemplateWorkspaces. So check if the type is not "template".
-            elif self.intended_workspace_type == "template":
-                raise ValidationError(
-                    {
-                        "intended_workspace_type": "intended_workspace_type may not be 'template'."
-                    }
-                )
+    intended_usage = models.TextField()
 
 
 class OpenAccessWorkspace(RequesterModel, TimeStampedModel, BaseWorkspaceData):
