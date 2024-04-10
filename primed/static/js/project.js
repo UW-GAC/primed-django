cc726f920e239fce01650fa67d1e098656d2c92a
Adrienne Stilp <amstilp@uw.edu>
2024-04-10 15:48:11 -0700

    Change javascript to copy workspaces input on click
    Move the button to the card subtitle instead of dynamically adding
    it via the javascript (as the blog example did). Move the javascript
    into the template inline_javascript block, since this code is only
    relevant for this template at the moment - there's no need to put
    it in the project-wide javascript. To make the javascript work,
    there must be:
    - an html element with the class "copy-element". This must have an
      attribute "copy-target" that specifies the id of the element whose
      text should be copied.
    - an element with the id specified in the "copy-target" attribute

--- primed/static/js/project.js
+++ primed/static/js/project.js
@@ -17,31 +17,3 @@ for(var i = 0; i < textInputs.length; i++){
   // Console: print the clicked <p> element
   textInputs[i].addEventListener("paste", checkPasteLength);
 }
-
-
-// Button to copy to the clipboard.
-const copyButtonLabel = "Copy Code";
-
-// use a class selector if available
-let blocks = document.querySelectorAll("pre");
-
-blocks.forEach((block) => {
-  // only add button if browser supports Clipboard API
-  if (navigator.clipboard) {
-    let button = document.createElement("button");
-
-    button.innerText = copyButtonLabel;
-    block.appendChild(button);
-
-    button.addEventListener("click", async () => {
-      await copyCode(block);
-    });
-  }
-});
-
-async function copyCode(block) {
-  let code = block.querySelector("code");
-  let text = code.innerText;
-
-  await navigator.clipboard.writeText(text);
-}
