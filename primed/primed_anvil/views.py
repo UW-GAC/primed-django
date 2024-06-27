f4e4813c27e2e4b06c021bd87781b69dbadd41be
Wienwipa Kirdpoo <wkirdpoo@uw.edu>
2024-06-27 14:20:56 -0700

    Define filter for StudyList Add StudyListFilter to StudyList view Add filter form to study_list template Add tests for StudyList view

--- primed/primed_anvil/views.py
+++ primed/primed_anvil/views.py
@@ -13,6 +13,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.messages.views import SuccessMessageMixin
 from django.db.models import Q
 from django.views.generic import CreateView, DetailView, TemplateView
+from django_filters.views import FilterView
 from django_tables2 import MultiTableMixin, SingleTableMixin, SingleTableView
 
 from primed.cdsa.models import DataAffiliateAgreement, MemberAgreement
@@ -34,7 +35,7 @@ from primed.miscellaneous_workspaces.tables import (
 )
 from primed.users.tables import UserTable
 
-from . import helpers, models, tables
+from . import filters, helpers, models, tables
 
 User = get_user_model()
 
@@ -77,11 +78,22 @@ class StudyDetail(AnVILConsortiumManagerViewRequired, MultiTableMixin, DetailVie
             )
 
 
-class StudyList(AnVILConsortiumManagerViewRequired, SingleTableView):
+class StudyList(AnVILConsortiumManagerViewRequired, SingleTableView, FilterView, autocomplete.Select2QuerySetView):
     """View to show a list of `Study`s."""
 
     model = models.Study
     table_class = tables.StudyTable
+    template_name = "primed_anvil/study_list.html"
+
+    filterset_class = filters.StudyListFilter
+
+    def get_queryset(self):
+        qs = models.Study.objects.order_by("short_name")
+
+        if self.q:
+            qs = qs.filter(Q(short_name__icontains=self.q) | Q(full_name__icontains=self.q))
+
+        return qs
 
 
 class StudyCreate(AnVILConsortiumManagerStaffEditRequired, SuccessMessageMixin, CreateView):
