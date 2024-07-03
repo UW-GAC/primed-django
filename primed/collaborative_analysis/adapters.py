d3474d992e4357802c245644af36ecadd0cdcab9
Adrienne Stilp <amstilp@uw.edu>
2024-07-03 09:05:54 -0700

    Use the WorkspaceAuthDomainDisabled form for dbGaP and collab adapters
    This disables the auth domain field, which is a better user
    experience when the auth domain will be automatically created.

--- primed/collaborative_analysis/adapters.py
+++ primed/collaborative_analysis/adapters.py
@@ -1,7 +1,7 @@
 from anvil_consortium_manager.adapters.workspace import BaseWorkspaceAdapter
-from anvil_consortium_manager.forms import WorkspaceForm
 
 from primed.primed_anvil.adapters import WorkspaceAdminSharingAdapter, WorkspaceAuthDomainAdapter
+from primed.primed_anvil.forms import WorkspaceAuthDomainDisabledForm
 
 from . import forms, models, tables
 
@@ -18,7 +18,7 @@ class CollaborativeAnalysisWorkspaceAdapter(
     description = "Workspaces used for collaborative analyses"
     list_table_class_staff_view = tables.CollaborativeAnalysisWorkspaceStaffTable
     list_table_class_view = tables.CollaborativeAnalysisWorkspaceUserTable
-    workspace_form_class = WorkspaceForm
+    workspace_form_class = WorkspaceAuthDomainDisabledForm
     workspace_data_model = models.CollaborativeAnalysisWorkspace
     workspace_data_form_class = forms.CollaborativeAnalysisWorkspaceForm
     workspace_detail_template_name = "collaborative_analysis/collaborativeanalysisworkspace_detail.html"
