3b0455199b973c870141b874d6f2acdd59efe931
Adrienne Stilp <amstilp@uw.edu>
2025-07-15 16:56:38 -0700

    Add anvil audit cache to settings files
    Add the same anvil_audit cache in the shared base.py settings file.
    Then add separate default caches in the local/test/prod settings files.

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,5 +1,5 @@
 from .base import *  # noqa
-from .base import env
+from .base import CACHES, env
 
 # GENERAL
 # ------------------------------------------------------------------------------
@@ -16,11 +16,9 @@ ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-        "LOCATION": "",
-    }
+CACHES["default"] = {
+    "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    "LOCATION": "",
 }
 
 # EMAIL
